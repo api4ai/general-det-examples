85e5bab380f904fdd96e30dade58d942f7e47d73
Jahangir Shah <shah@api4.ai>
2022-04-18 12:47:02 +0000

    Python requests example.

---
+++ python/requests/sample.py
@@ -0,0 +1,63 @@
+"""Example of using API4AI general objects detection."""
+
+import os
+import sys
+
+import requests
+
+
+# Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
+# For more details visit:
+#   https://api4.ai
+#
+# Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
+# For more details visit:
+#   https://rapidapi.com/api4ai-api4ai-default/api/general-detection/details
+MODE = 'demo'
+
+
+# Your RapidAPI key. Fill this variable with the proper value if you want
+# to try api4ai via RapidAPI marketplace.
+RAPIDAPI_KEY = ''
+
+
+OPTIONS = {
+    'demo': {
+        'url': 'https://demo.api4ai.cloud/general-det/v1/results',
+        'headers': {'A4A-CLIENT-APP-ID': 'sample'}
+    },
+    'rapidapi': {
+        'url': 'https://general-detection.p.rapidapi.com/v1/results',
+        'headers': {'X-RapidAPI-Key': RAPIDAPI_KEY}
+    }
+}
+
+
+if __name__ == '__main__':
+    # Parse args.
+    image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/general-det-1.jpg'
+
+    if '://' in image:
+        # POST image via URL.
+        response = requests.post(
+            OPTIONS[MODE]['url'],
+            headers=OPTIONS[MODE]['headers'],
+            data={'url': image})
+    else:
+        # POST image as file.
+        with open(image, 'rb') as image_file:
+            response = requests.post(
+                OPTIONS[MODE]['url'],
+                headers=OPTIONS[MODE]['headers'],
+                files={'image': (os.path.basename(image), image_file)}
+            )
+
+    # Print raw response.
+    print(f'💬 Raw response:\n{response.text}\n')
+
+    # Parse response and objects with confidence > 0.5.
+    confinend = [x['entities'][0]['classes']
+                 for x in response.json()['results'][0]['entities'][0]['objects']  # noqa
+                 if list(x['entities'][0]['classes'].values())[0] > 0.5]
+
+    print(f'💬 Objects with confidence above 0.5:\n{confinend}\n')
